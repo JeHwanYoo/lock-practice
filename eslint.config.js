c5dbb750f156e5e8f0ce59ff798ec3de89aef54e
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-05-28 23:35:13 +0900

    init: eslint for tdd

--- eslint.config.js
+++ eslint.config.js
@@ -8,4 +8,19 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   prettierConfigRecommended,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          'args': 'none',
+          'caughtErrors': 'all',
+          'caughtErrorsIgnorePattern': '^_',
+          'destructuredArrayIgnorePattern': '^_',
+          'varsIgnorePattern': '^_',
+          'ignoreRestSiblings': true,
+        },
+      ],
+    },
+  },
 ]
