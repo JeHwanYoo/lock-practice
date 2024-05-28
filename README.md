be2f607a0cc592a5a4adf58c78b74987819462f0
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-05-28 21:46:27 +0900

    init project

---
+++ README.md
@@ -0,0 +1,26 @@
+# DB Lock 연습
+
+## 진행도
+
+- [ ] Mongoose (MongoDB)
+  - [ ] 낙관적 락
+  - [ ] 비관적 락
+- [ ] TypeOrm (Postgres)
+  - [ ] 낙관적 락
+  - [ ] 비관적 락
+- [ ] Prisma (Postgres)
+  - [ ] 낙관적 락
+  - [ ] 비관적 락
+- [ ] Sequelize (Postgres)
+  - [ ] 낙관적 락
+  - [ ] 비관적 락
+- [ ] Redis
+  - [ ] 분산 락
+
+## 락의 종류
+
+### 낙관적 락
+
+### 비관적 락
+
+### 분산 락
---
+++ vitest.config.ts
@@ -0,0 +1,24 @@
+import swc from 'unplugin-swc'
+import {defineConfig} from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    globals: true,
+    root: './',
+    coverage: {
+      provider: 'v8',
+      // you can include other reporters, but 'json-summary' is required, json is recommended
+      reporter: ['text', 'json-summary', 'json'],
+      // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+      reportOnFailure: true,
+    },
+    pool: 'forks',
+  },
+  plugins: [
+    // This is required to build the test files with SWC
+    swc.vite({
+      // Explicitly set the module type to avoid inheriting this value from a `.swcrc` config file
+      module: {type: 'es6'},
+    }),
+  ],
+})
